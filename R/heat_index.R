ebce2f829923dc8546648cb652e02aba4796995a
Brooke Anderson <geanderson@jhsph.edu>
2015-12-18 11:34:53 -0700

    Added documentation for heat index functions.

--- R/heat_index.R
+++ R/heat_index.R
@@ -1,3 +1,54 @@
+#' Calculate heat index.
+#'
+#' \code{heat.index} creates a numeric vector of heat index values from
+#'    numeric vectors of air temperature and either relative humidity or
+#'    dew point temperature.
+#'
+#' @param t Numeric vector of air temperatures.
+#' @param dp Numeric vector of dew point temperatures.
+#' @param rh Numeric vector of relative humidity (in \%).
+#' @param temperature.metric Character string indicating the temperature
+#'    metric of air temperature and dew point temperature. Possible values
+#'    are 'fahrenheit' or 'celsius'.
+#' @param output.metric Character string indicating the metric into which
+#'    heat index should be calculated. Possible values are 'fahrenheit' or
+#'    'celcius'.
+#' @param round Integer indicating the number of decimal places to
+#'     round converted value.
+#'
+#' @details Include air temperature (\code{t}) and either dew point
+#'    temperature (\code{dp}) or relative humdity (\code{rh}). You cannot
+#'    specify both dew point temperature and relative humidity- this will
+#'    return an error. Heat index is calculated as \code{NA} when impossible
+#'    values of dew point temperature or humidity are input (e.g., humidity
+#'    above 100\% or below 0\%, dew point temperature above air temperature).
+#'
+#' @return A numeric vector of heat index values in the metric specified
+#'    by \code{output.metric}. (If \code{output.metric} is not specified,
+#'    heat index will be returned in the same metric in which air
+#'    temperature was input, specified by \code{temperature.metric}.)
+#'
+#' @note Equations are from the source code for the US National Weather
+#'     Service's
+#'     \href{http://www.wpc.ncep.noaa.gov/html/heatindex.shtml}{online heat index calculator}.
+#'
+#' @references
+#' Anderson GB, Bell ML, Peng RD. 2013. Methods to calculate the heat index
+#'    as an exposure Metric in environmental health research.
+#'    Environmental Health Perspectives 121(10):1111-1119.
+#'
+#' National Weather Service Hydrometeorological Prediction
+#'    Center Web Team. Heat Index Calculator. 30 Jan 2015.
+#'    \url{http://www.wpc.ncep.noaa.gov/html/heatindex.shtml}.
+#'    Accessed 18 Dec 2015.
+#'
+#' Rothfusz L. 1990. The heat index (or, more than you ever wanted to know
+#'    about heat index) (Technical Attachment SR 90-23). Fort Worth:
+#'    Scientific Services Division, National Weather Service.
+#'
+#' R. Steadman, 1979. The assessment of sultriness. Part I: A
+#'    temperature-humidity index based on human physiology and clothing
+#'    science. Journal of Applied Meteorology, 18(7):861--873.
 heat.index <-
         function (t = NA, dp = c(), rh = c(), temperature.metric = "fahrenheit",
                   output.metric = NULL, round = 0)
@@ -32,6 +83,45 @@ heat.index <-
                 return(hi)
         }
 
+#' Algorithm for heat.index function.
+#'
+#' \code{heat.index.algorithm} converts a numeric scalar of temperature
+#'    (in Fahrenheit) and a numeric scalar of relative humidity (in \%)
+#'    to heat index (in Fahrenheit). This function is not meant to be used
+#'    outside of the \code{\link{heat.index}} function.
+#'
+#' @param t Numeric scalar of air temperature, in Fahrenheit.
+#' @param rh Numeric scalar of relative humidity, in \%.
+#'
+#' @details If an impossible value of relative humidity is given
+#'    (below 0\% or above 100\%), heat index is returned as \code{NA}.
+#'
+#' @return A numeric scalar of heat index, in Fahrenheit.
+#'
+#' @note Equations are from the source code for the US National Weather
+#'     Service's
+#'     \href{http://www.wpc.ncep.noaa.gov/html/heatindex.shtml}{online heat index calculator}.
+#'
+#' @references
+#' Anderson GB, Bell ML, Peng RD. 2013. Methods to calculate the heat index
+#'    as an exposure Metric in environmental health research.
+#'    Environmental Health Perspectives 121(10):1111-1119.
+#'
+#' National Weather Service Hydrometeorological Prediction
+#'    Center Web Team. Heat Index Calculator. 30 Jan 2015.
+#'    \url{http://www.wpc.ncep.noaa.gov/html/heatindex.shtml}.
+#'    Accessed 18 Dec 2015.
+#'
+#' Rothfusz L. 1990. The heat index (or, more than you ever wanted to know
+#'    about heat index) (Technical Attachment SR 90-23). Fort Worth:
+#'    Scientific Services Division, National Weather Service.
+#'
+#' R. Steadman, 1979. The assessment of sultriness. Part I: A
+#'    temperature-humidity index based on human physiology and clothing
+#'    science. Journal of Applied Meteorology, 18(7):861--873.
+#'
+#' @seealso \code{\link{heat.index}}
+#'
 heat.index.algorithm <-
         function (t = NA, rh = NA)
         {
