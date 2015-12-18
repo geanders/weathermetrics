b59959d9cc5cb2de52cfe999a32cec4b0f8d4a7e
Brooke Anderson <geanderson@jhsph.edu>
2015-12-18 10:58:44 -0700

    Added roxygen2 documentation for fahrenheit.to.celsius.

--- R/temperature_conversions.R
+++ R/temperature_conversions.R
@@ -1,7 +1,7 @@
 #' Convert from Celsius to Fahrenheit.
 #'
-#' \code{celsius.to-fahrenheit} creates a vector of temperatures in
-#' Fahrenheit from a vector of temperatures in Celsius.
+#' \code{celsius.to.fahrenheit} creates a vector of temperatures in
+#'    Fahrenheit from a vector of temperatures in Celsius.
 #'
 #' @param T.celsius Numeric vector of temperatures in Celsius.
 #' @param round Integer indicating the number of decimal places to
@@ -22,6 +22,22 @@ celsius.to.fahrenheit <-
                 return(T.fahrenheit)
         }
 
+#' Convert from Fahrenheit to Celsius.
+#'
+#' \code{fahrenheit.to.celsius} creates a vector of temperatures in
+#'    Celsius from a vector of temperatures in Fahrenheit.
+#'
+#' @param T.fahrenheit Numeric vector of temperatures in Fahrenheit.
+#' @param round Integer indicating the number of decimal places to
+#'     round converted value.
+#'
+#' @return A vector of temperature values in Celsius.
+#'
+#' @note Equations are from the source code for the US National Weather
+#'     Service's
+#'     \href{http://www.wpc.ncep.noaa.gov/html/heatindex.shtml}{online heat index calculator}.
+#' @seealso \code{\link{celsius.to.fahrenheit}}
+#'
 fahrenheit.to.celsius <-
         function (T.fahrenheit, round = 2)
         {
---
+++ R/weathermetrics.R
@@ -0,0 +1,14 @@
+#' weathermetrics: Functions to convert between weather metrics
+#'
+#' The weathermetics package provides functions to convert between Celsius
+#'     and Fahrenheit, to convert between dew point temperature and relative
+#'     humidity, and to calculate heat index.
+#'
+#' @section weathermetrics functions:
+#' This package includes the following functions to calculate vectors of
+#'    weather metrics: 'celsius.to.fahrenheit', 'fahrenheit.to.celsius',
+#'    'dewpoint.to.humidity', 'humidity.to.dewpoint', and 'heat.index'.
+#'
+#' @docType package
+#' @name weathermetrics
+NULL
