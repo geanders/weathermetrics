492d00aa607e0fae3a5a3ca64b72e969a6b17faa
jferreri <joshua.m.ferreri@gmail.com>
2016-03-02 19:02:20 -0700

    added foco data. Created one file for all weathermetrics data

---
+++ R/weathermetrics_data.R
@@ -0,0 +1,61 @@
+#' Weather in Lyon, France
+#'
+#' Daily values of mean temperature (Celsius) and mean dew point temperature
+#' (Celsius) for the week of June 18, 2000, in Lyon, France.
+#'
+#' @source \href{http://www.wunderground.com/}{Weather Underground}
+#'
+#' @format A data frame with columns:
+#'    \describe{
+#'    \item{Date}{Date of weather observation}
+#'    \item{TemperatureC}{Daily mean temperature in Celsius}
+#'    \item{DewpointC}{Daily mean dewpoint temperature in Celsius}
+#'    }
+"lyon"
+
+#' Weather in New Haven, CT
+#'
+#' Daily values of mean temperature (Fahrenheit) and mean relative humidity
+#' (\%) for the week of October 19, 2008, in New Haven, CT.
+#'
+#' @source \href{http://www.wunderground.com/}{Weather Underground}
+#'
+#' @format A data frame with columns:
+#'    \describe{
+#'    \item{Date}{Date of weather observation}
+#'    \item{TemperatureF}{Daily mean temperature in Fahrenheit}
+#'    \item{Relative.Humidity}{Daily relative humidity in \%}
+#'    }
+"newhaven"
+
+#' Weather in Norfolk, VA
+#'
+#' Daily values of mean temperature (Fahrenheit) and mean dew point
+#' temperature (Fahrenheit) for the week of March 12, 2006, in Norfolk, VA.
+#'
+#' @source \href{http://www.wunderground.com/}{Weather Underground}
+#'
+#' @format A data frame with columns:
+#'    \describe{
+#'    \item{Date}{Date of weather observation}
+#'    \item{TemperatureF}{Daily mean temperature in Fahrenheit}
+#'    \item{DewpointF}{Daily mean dewpoint temperature in Fahrenheit}
+#'    }
+"norfolk"
+
+#' Weather in Suffolk, VA
+#'
+#' Daily values of mean temperature (Fahrenheit) and mean relative humidity
+#' (\%) for the week of July 12, 1998, in Suffolk, VA.
+#'
+#' @source \href{http://www.wunderground.com/}{Weather Underground}
+#'
+#' @format A data frame with columns:
+#'    \describe{
+#'    \item{Date}{Date of weather observation}
+#'    \item{TemperatureF}{Daily mean temperature in Fahrenheit}
+#'    \item{Relative.Humidity}{Daily relative humidity in \%}
+#'    }
+"suffolk"
+
+#'
