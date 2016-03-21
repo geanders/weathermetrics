f06651baa6f676ea341d2338b792ff557388062a
jferreri <joshua.m.ferreri@gmail.com>
2016-03-21 16:36:29 -0600

    added functions for precip conversions. Began vignette description. Added breck for inches.to.metric conversion

--- R/rainmeasure_conversion.R
+++ R/rainmeasure_conversion.R
@@ -1,4 +1,4 @@
-#' Convert between standard units of measure for precipitation
+#' Convert from inches to standard metric units of measure for precipitation
 #'
 #' \code{inches.to.metric} creates a numeric vector of precipitation in
 #'   common metric units (millimeters or centimeters) from a numeric vector of
@@ -20,7 +20,7 @@
 #' @return A numeric vector of precipitation (in specified metric unit)
 #'
 #' @details The numeric vector specified for unit must be in the units provided
-#'   as a character specified by unit.
+#'    as a character specified by unit.
 #'
 #' @note Equations are from the source code for the National Weather Service
 #'    Forecast Office \href{http://www.srh.noaa.gov/ama/?n=conversions}
@@ -35,9 +35,7 @@
 #'\url{http://www.srh.noaa.gov/ama/?n=conversions}
 #'
 #' @examples
-#' data(beijing)
-#' beijing$knots <- speed.to.knots(beijing$kmph, "kmph", r = TRUE)
-#' beijing
+
 #'
 #' @export
 inches.to.metric <- funciton(inches, unit, round.out = TRUE, round = 2){
@@ -57,6 +55,46 @@ inches.to.metric <- funciton(inches, unit, round.out = TRUE, round = 2){
         stop("unit must be in mm or cm")
 }
 
+#' Convert between standard metric units of measure for precipitation to inches
+#'
+#' \code{metric.to.inches} creates a numeric vector of precipitation measures in
+#'   inches from a numeric vector of precipitation in common metric units
+#'   (millimeters or centimeters).
+#'
+#' @param metric Numeric vector of precipitation (in millimeters or centimeters)
+#' @param unit.from Character specifying the metric precipitation unit convert
+#'   from to inches.
+#'   Possible values are:
+#'     \itemize{
+#'       \item \code{mm}: Millimeters
+#'       \item \code{cm}: Centimeters
+#'     }
+#' @param round.out Variable indicating whether or not to round results.
+#'     Possible values are TRUE or FALSE. Default value is TRUE.
+#' @param round Integer indicating the number of decimal places to
+#'     round converted value. Default value is 1.
+#'
+#' @return A numeric vector of precipitation in inches.
+#'
+#' @details The numeric vector specified for unit.from must be in the units
+#'    provided as a character specified by unit.from.
+#'
+#' @note Equations are from the source code for the National Weather Service
+#'    Forecast Office \href{http://www.srh.noaa.gov/ama/?n=conversions}
+#'
+#' @author
+#' Brooke Anderson \email{brooke.anderson@@colostate.edu},
+#' Joshua Ferreri \email{joshua.m.ferreri@@gmail.com}
+#'
+#' @seealso \code{\link{inches.to.metric}}
+#'
+#' @references
+#'\url{http://www.srh.noaa.gov/ama/?n=conversions}
+#'
+#' @examples
+
+#'
+#' @export
 metric.to.inches <- function(metric, unit.from, round.out = TRUE, round = 2){
         if(unit.from == "mm" & round.out == TRUE){
                 inches <- metric / 25.4
