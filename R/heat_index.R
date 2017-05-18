faed45101ec5e8e46bcb78cc7815e22d5879d955
Doug Friedman <sillyville+github@gmail.com>
2017-05-17 23:02:35 -0400

    Slight performance improvements
    - Replaced mapply with .mapply
    - Used double logical comparisons for scalars
    - Replaced length > 0 with any

--- R/heat_index.R
+++ R/heat_index.R
@@ -80,26 +80,28 @@ heat.index <-
                 if(length(output.metric) == 0){
                         output.metric <- temperature.metric
                 }
-                if (length(dp) == 0 & length(rh) == 0) {
+                if (length(dp) == 0 && length(rh) == 0) {
                         stop("You must give values for either dew point temperature ('dp') or relative humidity ('rh').")
                 }
-                else if (length(dp) > 0 & length(rh) > 0) {
+                else if (length(dp) > 0 && length(rh) > 0) {
                         stop("You can give values for either dew point temperature ('dp') or relative humidity ('rh'), but you cannot specify both to this function.")
                 }
-                if (length(dp) != length(t) & length(rh) != length(t)) {
+                if (length(dp) != length(t) && length(rh) != length(t)) {
                         stop("The vectors for temperature ('t') and moisture (either relative humidity, 'rh', or dew point temperature, 'dp') must be the same length.")
                 }
                 if (length(dp) > length(rh)) {
                         rh <- dewpoint.to.humidity(t = t, dp = dp, temperature.metric = temperature.metric)
                 }
-                else if (length(rh[!is.na(rh) & (rh > 100 | rh < 0)]) > 0) {
+                else if (any(!is.na(rh) & (rh > 100 | rh < 0))) {
                         rh[!is.na(rh) & (rh > 100 | rh < 0)] <- NA
                         warning("There were observations with an impossible values for relative humidity (below 0% or above 100%). For these observations, heat index was set to NA.")
                 }
                 if (temperature.metric == "celsius") {
                         t <- celsius.to.fahrenheit(t)
                 }
-                hi <- mapply(heat.index.algorithm, t = t, rh = rh)
+
+                hi <- unlist(.mapply(heat.index.algorithm, dots=list(t, rh), MoreArgs=NULL))
+
                 if (output.metric == "celsius") {
                         hi <- fahrenheit.to.celsius(hi)
                 }
