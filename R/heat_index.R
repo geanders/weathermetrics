42051b3fe48ee23e0340b4d25150e9bffcd9d85f
Doug Friedman <sillyville+github@gmail.com>
2017-05-17 23:03:48 -0400

    Adding double logical operators for scalars

--- R/heat_index.R
+++ R/heat_index.R
@@ -156,7 +156,7 @@ heat.index <-
 heat.index.algorithm <-
         function (t = NA, rh = NA)
         {
-                if (is.na(rh) | is.na(t)) {
+                if (is.na(rh) || is.na(t)) {
                         hi <- NA
                 } else if (t <= 40) {
                         hi <- t
@@ -169,12 +169,12 @@ heat.index.algorithm <-
                                         5.481717 * 10^-2 * rh^2 + 1.22874 * 10^-3 * t^2 *
                                         rh + 8.5282 * 10^-4 * t * rh^2 - 1.99 * 10^-6 *
                                         t^2 * rh^2
-                                if (rh <= 13 & t >= 80 & t <= 112) {
+                                if (rh <= 13 && t >= 80 && t <= 112) {
                                         adjustment1 <- (13 - rh)/4
                                         adjustment2 <- sqrt((17 - abs(t - 95))/17)
                                         total.adjustment <- adjustment1 * adjustment2
                                         hi <- hi - total.adjustment
-                                } else if (rh > 85 & t >= 80 & t <= 87) {
+                                } else if (rh > 85 && t >= 80 && t <= 87) {
                                         adjustment1 <- (rh - 85)/10
                                         adjustment2 <- (87 - t)/5
                                         total.adjustment <- adjustment1 * adjustment2
